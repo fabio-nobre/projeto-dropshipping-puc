94439ea2edae477c786331ce931ca0f0fdc0a0ee
Fabio Nobre <f.oliveiranobre@gmail.com>
2019-03-07 20:47:40 -0300

    alterando aplicacao principal rename

--- src/main/java/com/fabionobre/loja/AplicacaoPrincipal.java
+++ src/main/java/com/fabionobre/loja/AplicacaoPrincipal.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class AplicacaoPrincipal implements CommandLineRunner {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
+		SpringApplication.run(AplicacaoPrincipal.class, args);
 	}
 
 	@Override

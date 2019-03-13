b0aa5c484852aa2988a09d34ff72353ece5bc03e
Fabio Nobre <f.oliveiranobre@gmail.com>
2019-03-13 19:54:54 -0300

    Ajustes

--- src/main/java/com/fabionobre/loja/config/SecurityConfig.java
+++ src/main/java/com/fabionobre/loja/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		// Autenticacão usuario
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/fabionobre/loja/services/UserService.java
+++ src/main/java/com/fabionobre/loja/services/UserService.java
@@ -4,10 +4,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.fabionobre.loja.security.UserSS;
 
+// Recupera usuário logado
 public class UserService {
 	
 	public static UserSS authenticated() {
 		try {
+			// Retornar usuário logado
 			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		}
 		catch (Exception e) {
